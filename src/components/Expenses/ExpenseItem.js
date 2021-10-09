cb82a6ad915cb52501b1d77255e6fd2aed4755e6
Ivan Lytvynenko <ivan.lytvynenko.dev@gmail.com>
2021-10-09 18:16:59 +0300

    Listening to Events & Working with Event Handlers
    This code doesn't change view, because state is needed here

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,13 +3,21 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = 'New title';
+    console.log('Updated!');
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   )
 }
