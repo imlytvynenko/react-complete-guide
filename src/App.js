0c1f5538ef5ebee26d8e5cb5dc9532d4e6882636
Ivan Lytvynenko <ivan.lytvynenko.dev@gmail.com>
2021-10-18 22:40:04 +0300

    Adding Form Inputs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import NewExpense from "./components/NewExpense/NewExpense";
 import ExpensesList from "./components/Expenses/ExpenseList";
 
 const App = () => {
@@ -25,7 +26,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <ExpensesList items={expenses} />
     </div>
   );
